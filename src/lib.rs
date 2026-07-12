2d698f083b90bb32ca58871a7dfb07968c83474f
KUANG TA LIN <kuangtalin@KUANGdeMacBook-Air.local>
2026-07-12 13:34:45 +0800

    Test: Setup real covopt benchmark target

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,24 @@ extern crate alloc;
 pub mod instruction;
 pub mod vm;
 pub mod assembler;
+
+#[cfg(test)]
+extern crate std;
+
+#[cfg(test)]
+mod covopt_tests {
+    use std::env;
+    use super::*;
+
+    #[test]
+    fn covopt_benchmark_test() {
+        let n_str = env::var("COVOPT_N").unwrap_or_else(|_| alloc::string::String::from("100"));
+        let n: usize = n_str.parse().unwrap();
+        
+        // Simulating O(N) behavior based on n
+        let mut _sum = 0;
+        for i in 0..n { // TARGET LINE
+            _sum += i;
+        }
+    }
+}
